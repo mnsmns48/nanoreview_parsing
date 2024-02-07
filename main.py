700186a0a53c24ad1515e586159172f75eab73bf
mnsmns48 <getzboy@list.ru>
2024-02-07 16:56:09 +0300

    add logger

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import asyncio
 
 from core import full_scrape
+from logger import logger
 
 
 async def main():
-    await full_scrape()
-    # print('If you want to parse the entire site, press "1", if you want to parse a separate link, then press "2"')
-    # try:
-    #     choice = int(input())
-    #     if choice == 1:
-    #
-    #     if choice == 2:
-    #         pass
-    #         # await one_link()
-    #     else:
-    #         print('Incorrect input, you must press "1" or "2"')
-    # except ValueError:
-    #     print('Incorrect input!')
+    print('If you want to parse the entire site, press "1", if you want to parse a separate link, then press "2"')
+    try:
+        choice = int(input())
+        if choice == 1:
+            await full_scrape()
+        if choice == 2:
+            pass
+            # await one_link()
+        else:
+            print('Incorrect input, you must press "1" or "2"')
+    except BaseException as e:
+        logger.debug(e)
 
     await asyncio.sleep(0.1)
 
